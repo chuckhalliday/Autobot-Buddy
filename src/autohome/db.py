55d9e539f43e4bde86c456a72df618e1c17acffa
chuckhalliday <charleshallidayclarkii@gmail.com>
2024-01-24 22:08:32 -0800

    Add database

---
+++ src/autohome/db.py
@@ -0,0 +1,12 @@
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'bucket',
+    'USER': 'chuckhalliday',
+    'PASSWORD': 'qirhWB4Zb5xO',
+    'HOST': 'ep-nameless-silence-a61mntpq.us-west-2.aws.neon.tech',
+    'PORT': '5432',
+    'OPTIONS': {'sslmode': 'require'},
+  }
+}
