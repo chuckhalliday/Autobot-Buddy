c13a7ce782aa7d91e4a23d118eefb5982b1129c6
chuckhalliday <charleshallidayclarkii@gmail.com>
2024-02-04 23:04:23 -0800

    Add chatbot logic template

--- src/queries.py
+++ src/queries.py
@@ -6,6 +6,7 @@ from langchain.text_splitter import CharacterTextSplitter # for splitting text i
 import os # for reading environment variables
 from dotenv import load_dotenv # for loading environment variables
 from openai import OpenAI # openai api
+from numpy import dot
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
@@ -135,3 +136,83 @@ def create_embeddings(file_path: str, vehicle: str, obj: str):
         print(f"File '{file_name}' already exists. Skipping creation.")
         attachment_instance = VehicleAttachment(vehicle=obj, file=file_instance)
         attachment_instance.save()
+
+
+def get_embeddings(extracted_json_path: str):
+    
+    # Open the JSON file containing embeddings and snippets
+    with open(extracted_json_path,'r') as file:
+        # Load the JSON data into a Python dictionary
+        embedding_json = json.load(file)
+        
+    # Return the embeddings and snippets from the loaded JSON
+    return embedding_json['embeddings'], embedding_json['snippets']
+
+
+def user_question_embedding_creator(question: str):
+    
+    client = OpenAI(api_key=OPENAI_API_KEY)
+    
+    # Request embedding for the provided question using the specified model
+    response = client.embeddings.create(input=question,model=EMBEDDING_MODEL)
+    
+    # Extract and return the embedding from the API response
+    return response.data[0].embedding
+
+
+def answer_users_question(user_question, embeddings, snippets, gpt_model):
+    
+    try:
+        # Create an embedding for the user's question
+        user_question_embedding = user_question_embedding_creator(user_question)
+    except Exception as e:
+        # Handle any exception that occurred while using Embedding API.
+        return f"An error occurred while creating embedding: {str(e)}"
+        
+    
+    # Calculate cosine similarities between the user's question embedding and the document embeddings
+    cosine_similarities = []
+    for embedding in embeddings:
+        cosine_similarities.append(dot(user_question_embedding,embedding))
+
+    # Pair snippets with their respective cosine similarities and sort them by similarity
+    scored_snippets = zip(snippets, cosine_similarities)
+    sorted_snippets = sorted(scored_snippets, key=lambda x: x[1], reverse=True)
+    
+    # Filter snippets based on a confidence score and select the top 5 results
+    formatted_top_results = [snipps for snipps, _score in sorted_snippets if _score > CONFIDENCE_SCORE]
+    if len(formatted_top_results) > 5:
+        formatted_top_results = formatted_top_results[:5]
+        
+    # Create the chatbot system using pdf_description provided by the user.
+    chatbot_system = f"""You are provided with SEARCH RESULTS from a pdf. You need to generate answer to the user's question based on the given SEARCH RESULTS. SEARCH RESULTS as a python list. SEARCH RESULTS and USER's QUESTION are delimited by ``` \n If there is no information available, or question is irrelevent respond with - "Sorry! I can't help you." """
+    
+    # Create the prompt using results and user's question.
+    prompt = f"""\
+    SEARCH RESULTS:
+    ```
+    {formatted_top_results}
+    ```
+    USER'S QUESTION:
+    ```
+    {user_question}
+    ```
+    
+    """
+    
+    # Prepare the chat conversation and use GPT model for generating a response
+    messages = [{'role':'system', 'content':chatbot_system},
+                {'role':'user', 'content':prompt}]
+    
+    try:
+        client = OpenAI(api_key=OPENAI_API_KEY)
+        completion = client.chat.completions.create(model=gpt_model,
+                                             messages=messages,
+                                             temperature=0,
+                                             stream=False)
+    except Exception as e:
+        # Handle exception while communicating with ChatCompletion API
+        return f"An error occurred with chatbot: {str(e)}"
+        
+    # Return the chatbot response.
+    return completion.choices[0].message.content
